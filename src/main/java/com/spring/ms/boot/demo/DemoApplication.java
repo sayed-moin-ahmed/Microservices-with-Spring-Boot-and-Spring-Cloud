9bbe5d2c0e9c2d99ca16aa35ec39a6f715c11914
sayedmoinahmed <s3dm2na3d@gmail.com>
2021-08-15 19:08:37 +0530

    modifications

--- src/main/java/com/spring/ms/boot/demo/DemoApplication.java
+++ src/main/java/com/spring/ms/boot/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.microservices.springcloud.springboot;
+package com.spring.ms.boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
